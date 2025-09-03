8cfaddeba074c0e304efdd29f822bfec4ecd72ad
subin <han04245@naver.com>
2025-09-03 16:55:18 +0900

    Initial commit

---
+++ src/main/java/sandri/sandriweb/SandriWebApplication.java
@@ -0,0 +1,13 @@
+package sandri.sandriweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandriWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandriWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sandri/sandriweb/SandriWebApplicationTests.java
@@ -0,0 +1,13 @@
+package sandri.sandriweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandriWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
